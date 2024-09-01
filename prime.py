cbf6fa507eab5bbca8e850a4a4fdd6916e276d5d
prasadkartikeya <prasad.kartikeya@gmail.com>
2024-09-01 10:36:33 -0700

    task3

--- prime.py
+++ prime.py
@@ -1,4 +1,10 @@
-n = int(input())
-
+n = int(input("enter number: "))
 # Write code below here
+for i in range (2,n):
+    if n % i == 0:
+        print(n,"is not a prime")
+        break
+    else:
+        print(n,"is a prime number")
 # Print the n'th prime number
+ 
