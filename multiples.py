57d8cfa0488b20e427bcf2c3f8207146856d2015
prasadkartikeya <prasad.kartikeya@gmail.com>
2024-09-01 08:18:51 -0700

    task

--- multiples.py
+++ multiples.py
@@ -6,3 +6,41 @@ The sum of these multiples is 23.
 
 Find and print the sum of all the multiples of 3 or 5 below 1000.
 """
+#variable sorting total
+total=0
+# using for loop
+for i in range(1000):
+    #check reminder is zero or not
+    if i%3 == 0 or i% 5== 0:
+        total = total + i 
+        print(total)
+#Sum of all the multiples of 3 or 5 below 1000 =233168
+
+
+ 
+    
+
+
+
+
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
