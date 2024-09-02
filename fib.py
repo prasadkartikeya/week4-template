bab267c3e272e0a850ca7010caa0d305e75201be
prasadkartikeya <prasad.kartikeya@gmail.com>
2024-09-01 20:14:45 -0700

    task

--- fib.py
+++ fib.py
@@ -1,4 +1,16 @@
-n = int(input())
 
 # Write code below here
-# Print the n'th fibonacci number
+n = int(input("enter number: "))
+if(n<=0):
+    print("invalid")
+if(n==1):
+    print(0)
+if(n>=2):
+    a=0
+    b=1
+    for i in range(0,n):
+        c=a+b
+        print(c)
+        a=b
+        b=c
+# Print the n'th fibon
